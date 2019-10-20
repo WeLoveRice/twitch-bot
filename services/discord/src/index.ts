ef2decd69e6b847b2f5bf617060a742991e7192d
Colin Cheung <ccheung@podfather.com>
2019-10-20 15:41:49 +0100

    Setup eslint with vscode

--- services/discord/src/index.ts
+++ services/discord/src/index.ts
@@ -1,15 +1,22 @@
-import Discord from 'discord.js'
+import Discord from "discord.js";
 
-const client = new Discord.Client()
+const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong')
+client.on("ready", () => {
+  const { user } = client;
+  if (!user) {
+    return;
   }
-})
+  console.log(`Logged in as ${user.tag}!`);
+});
 
-client.login('token')
+client.on("message", msg => {
+  if (typeof msg.reply !== "function") {
+    return;
+  }
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+});
+
+client.login("NjM1NDcwMjg0MTI3ODYyNzk1.Xaxi3Q.NwIPq5f0eucSvLByAZxSJCprmiI");
