42de00e60668d0f2b068ab6ca296849192fcb2e6
Colin Cheung <ccheung@podfather.com>
2019-11-19 00:16:22 +0000

    Add tests

--- services/discord/src/Logger.ts
+++ services/discord/src/Logger.ts
@@ -1,18 +1,25 @@
 import winston, { Logger, format, transports, config } from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
+import path from "path";
 
-const dailyRotate = new DailyRotateFile({
-  filename: "./logs/%DATE%.log",
-  datePattern: "YYYY-MM-DD HH:00",
-  zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "14d"
-});
+export const LOG_DIR = path.join(__dirname, "..", "logs");
 
-export const createLogger = (): Logger => {
+const dailyRotate = (fileName = "%DATE%"): DailyRotateFile => {
+  return new DailyRotateFile({
+    filename: `${LOG_DIR}/${fileName}.log`,
+    datePattern: "YYYY-MM-DD HH:00",
+    zippedArchive: true,
+    maxSize: "20m",
+    maxFiles: "14d"
+  });
+};
+
+export const createLogger = (fileName: string): Logger => {
+  const { Console } = transports;
+
   return winston.createLogger({
     levels: config.syslog.levels,
     format: format.combine(format.timestamp(), format.json()),
-    transports: [new transports.Console({ level: "error" }), dailyRotate]
+    transports: [new Console({ level: "error" }), dailyRotate(fileName)]
   });
 };
