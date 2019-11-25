97d819527ac3eb8486dbc1fdab89b4834283d869
Colin Cheung <ccheung@podfather.com>
2019-11-25 00:28:42 +0000

    Fix test after refactoring

--- services/discord/tests/periodicTask/Runner.test.ts
+++ services/discord/tests/periodicTask/Runner.test.ts
@@ -1,7 +1,7 @@
 import { VoiceChannelDisconnect } from "../../src/periodicTask/VoiceChannelDisconnect";
 import { Runner } from "../../src/periodicTask/Runner";
 
-jest.mock("./../../src/PeriodicTask/VoiceChannelDisconnect");
+jest.mock("./../../src/periodicTask/VoiceChannelDisconnect");
 
 const mockPeriodicTask = VoiceChannelDisconnect as jest.Mock<
   VoiceChannelDisconnect
@@ -14,21 +14,19 @@ beforeEach(() => {
   mockPeriodicTask.mockClear();
 });
 
-describe("Runner Test", () => {
-  it("starts the timer when start is called", () => {
-    // For some reason, can't mock periodic task execute to return a promise
-    // and then assert the runner timer is null
-    periodicTask = new mockPeriodicTask();
-    runner = new Runner();
+it("starts the timer when start is called", () => {
+  // For some reason, can't mock periodic task execute to return a promise
+  // and then assert the runner timer is null
+  periodicTask = new mockPeriodicTask();
+  runner = new Runner();
 
-    expect(runner.timer).toBe(null);
-    runner.start(periodicTask);
+  expect(runner.timer).toBe(null);
+  runner.start(periodicTask);
 
-    jest.runOnlyPendingTimers();
-    expect(runner.timer).not.toBe(null);
-    expect(periodicTask.execute).toBeCalledTimes(1);
+  jest.runOnlyPendingTimers();
+  expect(runner.timer).not.toBe(null);
+  expect(periodicTask.execute).toBeCalledTimes(1);
 
-    runner.stop();
-    expect(runner.timer).toBe(null);
-  });
+  runner.stop();
+  expect(runner.timer).toBe(null);
 });
