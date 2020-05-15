4198aa08c34fc4f4930872069764e8aa3f796ea7
Colin Cheung <colin.cheung@skyscanner.net>
2020-05-15 14:35:40 +0100

    Fix join voice channel test

--- services/discord/src/commands/JoinVoiceChannel.ts
+++ services/discord/src/commands/JoinVoiceChannel.ts
@@ -1,45 +1,29 @@
 import { Bot } from "./../enum/Bot";
-import { GuildMember, VoiceChannel, Message } from "discord.js";
+import { Message } from "discord.js";
 import { AbstractCommand } from "./AbstractCommand";
 import { VoiceChannelManager } from "../VoiceChannelManager";
-
-export const isMemberInVoiceChannel = async (
-  message: Message
-): Promise<boolean> => {
-  if (!message.member) {
-    return false;
-  }
-
-  const { channel } = message.member.voice;
-  if (!(channel instanceof VoiceChannel)) {
-    await message.reply("You must be in a voice channel for me to join");
-    return false;
-  }
-
-  return true;
-};
+import {
+  isMemberInVoiceChannel,
+  getVoiceChannelFromMessage
+} from "../api/discord/VoiceChannel";
 
 export class JoinVoiceChannel extends AbstractCommand {
-  public getVoiceChannelFromMessage(): VoiceChannel {
-    if (!this.message.member?.voice?.channel) {
-      throw new ReferenceError(
-        "member or voice does does not exist on message"
-      );
-    }
-
-    return this.message.member.voice.channel;
-  }
-
   public async isValid(): Promise<boolean> {
     return isMemberInVoiceChannel(this.message);
   }
 
   protected async run(): Promise<void> {
     try {
-      const channel = this.getVoiceChannelFromMessage();
+      const channel = getVoiceChannelFromMessage(this.message);
+      if (!channel) {
+        await this.message.reply(
+          "You must be in a voice channel for me to join"
+        );
+        return;
+      }
 
       if (channel.members.has(Bot.USER_ID)) {
-        this.message.reply("Already in channel");
+        await this.message.reply("Already in channel");
         return;
       }
 
