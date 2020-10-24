0731126621fa3fc2fed44a7b601eb57c8e00597d
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-24 19:43:46 +0100

    add api tests

--- services/discord/src/api/redis.ts
+++ services/discord/src/api/redis.ts
@@ -5,14 +5,15 @@ const connect = () => {
   const logger = createLogger();
   const redis = new Redis(6379, "redis");
 
-  redis.on("error", error => {
-    logger.error(`redis error: ${error}`);
-  });
-
   redis.on("ready", async () => {
     await redis.flushall();
     logger.info("Connected to redis");
   });
+
+  redis.on("error", error => {
+    logger.error(`redis error: ${error}`);
+  });
+
   return redis;
 };
 
---
+++ services/discord/tests/api/discord/index.test.ts
@@ -0,0 +1,19 @@
+import * as Discord from "discord.js";
+import discordApi from "../../../src/api/discord";
+
+jest.mock("discord.js");
+jest.mock("../../../src/commands/Factory");
+jest.mock("../../../src/Logger");
+jest.mock("../../../src/api/postgres");
+jest.mock("../../../src/api/redis");
+
+it("runs as expected", () => {
+  discordApi.connect();
+
+  const discordMock = Discord as jest.Mocked<typeof Discord>;
+  const clientEventListener = discordMock.Client.mock.instances[0].on;
+
+  expect(clientEventListener).toBeCalledWith("ready", expect.any(Function));
+  expect(clientEventListener).toBeCalledWith("message", expect.any(Function));
+  expect(clientEventListener).toBeCalledWith("error", expect.any(Function));
+});
