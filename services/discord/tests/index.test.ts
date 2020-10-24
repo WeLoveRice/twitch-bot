0f1788a94723f6395c5ace9ec530172cf05c35a0
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-24 18:58:51 +0100

    add expects to index test

--- services/discord/tests/index.test.ts
+++ services/discord/tests/index.test.ts
@@ -1,4 +1,7 @@
 import { main } from "../src/index";
+import postgres from "../src/api/postgres";
+import discord from "../src/api/discord";
+import redis from "../src/api/redis";
 
 jest.mock("../src/Logger", () => ({
   createLogger: jest.fn().mockReturnValue({
@@ -12,4 +15,8 @@ jest.mock("../src/api/redis");
 
 it("runs expected", async () => {
   await main();
+
+  expect(postgres.connect).toBeCalled();
+  expect(discord.connect).toBeCalled();
+  expect(redis.connect).toBeCalled();
 });
