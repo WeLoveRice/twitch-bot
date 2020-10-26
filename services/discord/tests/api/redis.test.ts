e27b59d86e5800b5b62e5cd2009be9c00182cc21
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-26 01:29:40 +0000

    fix redis test

--- services/discord/tests/api/redis.test.ts
+++ services/discord/tests/api/redis.test.ts
@@ -1,18 +1,26 @@
-import redis from "../../src/api/redis";
-import * as ioredis from "ioredis";
+import { Redis as RedisApi } from "../../src/api/redis";
+import * as Redis from "ioredis";
 
 jest.mock("discord.js");
 jest.mock("../../src/Logger");
 jest.mock("ioredis");
 
-it("runs as expected", async () => {
-  expect(await redis.connect()).toBeInstanceOf(ioredis.default);
+describe("connect", () => {
+  it("runs as expected", async () => {
+    expect(RedisApi.connect()).toBeInstanceOf(Redis.default);
 
-  const ioredisMock = ioredis as jest.Mocked<typeof ioredis>;
+    const ioredisMock = Redis as jest.Mocked<typeof Redis>;
 
-  expect(ioredisMock.default).toBeCalledWith(6379, "redis");
+    expect(ioredisMock).toBeCalledWith(6379, "redis");
 
-  const poolInstance = ioredisMock.default.mock.instances[0];
-  expect(poolInstance.on).toBeCalledWith("ready", expect.any(Function));
-  expect(poolInstance.on).toBeCalledWith("error", expect.any(Function));
+    const poolInstance = ioredisMock.default.mock.instances[0];
+    expect(poolInstance.on).toBeCalledWith("ready", expect.any(Function));
+    expect(poolInstance.on).toBeCalledWith("error", expect.any(Function));
+  });
+});
+
+describe("getConnection", () => {
+  it("returns a redis connection", () => {
+    expect(RedisApi.getConnection()).toBeInstanceOf(Redis.default);
+  });
 });
