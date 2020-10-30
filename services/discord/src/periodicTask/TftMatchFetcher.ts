48bb79d7eeebb4769490d4121b8735dcc50c3278
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-30 17:55:19 +0000

    add basic test for tft match fetcher

--- services/discord/src/periodicTask/TftMatchFetcher.ts
+++ services/discord/src/periodicTask/TftMatchFetcher.ts
@@ -1,9 +1,10 @@
 import { Logger } from "winston";
 import { PeriodicTask } from ".";
+import { TftSummoner } from "../../models/TftSummoner";
 import { getMatchDetail, getParticipantFromMatch } from "../api/riot";
 import { createLogger } from "../Logger";
 import {
-  fetchLatestUnprocessedMatch,
+  fetchLatestUnprocessedMatchId,
   findSummonerByName,
   insertMatchDetail,
   insertMatchHistory,
@@ -13,30 +14,41 @@ import {
 
 export class TftMatchFetcher implements PeriodicTask {
   summonerName: string;
+  summoner!: TftSummoner;
   interval = 30 * 1000;
   logger: Logger;
+
   public constructor(summonerName: string) {
     this.summonerName = summonerName;
     this.logger = createLogger();
   }
 
-  async execute(): Promise<boolean> {
-    const summoner = await findSummonerByName(this.summonerName);
-    const latestMatch = await fetchLatestUnprocessedMatch(summoner);
-    if (!latestMatch) {
-      return false;
+  async initialiseSummoner(): Promise<void> {
+    if (!this.summoner) {
+      this.summoner = await findSummonerByName(this.summonerName);
     }
-
-    const match = await getMatchDetail(latestMatch);
+  }
+  async insertDataForMatch(matchId: string): Promise<void> {
+    const match = await getMatchDetail(matchId);
     if (!(await matchDetailExists(match))) {
       await insertMatchDetail(match);
     }
-    const participant = await getParticipantFromMatch(match, summoner);
+    const participant = await getParticipantFromMatch(match, this.summoner);
     const participantResult = await insertParticipantResult(
       participant,
-      summoner
+      this.summoner
     );
-    await insertMatchHistory(participantResult, match, summoner);
+    await insertMatchHistory(participantResult, match, this.summoner);
+  }
+
+  async execute(): Promise<boolean> {
+    await this.initialiseSummoner();
+    const matchId = await fetchLatestUnprocessedMatchId(this.summoner);
+    if (!matchId) {
+      return false;
+    }
+
+    await this.insertDataForMatch(matchId);
     return false;
   }
 }
