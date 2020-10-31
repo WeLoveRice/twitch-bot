5198a107b651aaa5d5be70846ee6f1b8a9c5d2fe
Colin Cheung <ColinCee@users.noreply.github.com>
2020-10-31 19:40:45 +0000

    update table

--- services/discord/models/TftMatchHistory.ts
+++ services/discord/models/TftMatchHistory.ts
@@ -3,10 +3,10 @@
 import { DataTypes, Model, Optional, Sequelize } from "sequelize";
 
 export interface TftMatchHistoryAttributes {
-  id: number;
-  tftParticipantResultId: number;
-  tftMatchDetailsRiotId: string;
-  tftSummonerId: string;
+  id?: number;
+  tftParticipantResultId?: number;
+  tftMatchDetailsRiotId?: string;
+  tftSummonerId?: number;
 }
 
 export type TftMatchHistoryCreationAttributes = Optional<
@@ -15,12 +15,12 @@ export type TftMatchHistoryCreationAttributes = Optional<
 >;
 
 export class TftMatchHistory
-  extends Model<TftMatchHistoryAttributes, TftMatchHistoryCreationAttributes>
+  extends Model<TftMatchHistoryAttributes, TftMatchHistoryAttributes>
   implements TftMatchHistoryAttributes {
-  id!: number;
-  tftParticipantResultId!: number;
-  tftMatchDetailsRiotId!: string;
-  tftSummonerId!: string;
+  id?: number;
+  tftParticipantResultId?: number;
+  tftMatchDetailsRiotId?: string;
+  tftSummonerId?: number;
 
   static initModel(sequelize: Sequelize) {
     TftMatchHistory.init(
@@ -34,16 +34,29 @@ export class TftMatchHistory
         tftParticipantResultId: {
           type: DataTypes.INTEGER,
           allowNull: false,
+          references: {
+            model: "tft_participant_result",
+            key: "id"
+          },
+          unique: "tft_participant_result_id_unq",
           field: "tft_participant_result_id"
         },
         tftMatchDetailsRiotId: {
           type: DataTypes.TEXT,
           allowNull: false,
+          references: {
+            model: "tft_match_details",
+            key: "riot_id"
+          },
           field: "tft_match_details_riot_id"
         },
         tftSummonerId: {
           type: DataTypes.INTEGER,
           allowNull: false,
+          references: {
+            model: "tft_summoner",
+            key: "id"
+          },
           field: "tft_summoner_id"
         }
       },
