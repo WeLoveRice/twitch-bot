8d0a0a7ca2ef9c24f9e2507e2a34823ca653995f
Colin Cheung <ColinCee@users.noreply.github.com>
2021-02-16 22:52:19 +0000

    add help command

---
+++ services/discord/src/commands/explicit/Help.ts
@@ -0,0 +1,16 @@
+import { Command } from "../../enum/CommandEnum";
+import { AbstractCommand } from "../AbstractCommand";
+
+export class Help extends AbstractCommand {
+  public async isValid(): Promise<boolean> {
+    return true;
+  }
+  protected async run(): Promise<void> {
+    const formattedList = Object.values(Command)
+      .filter(value => value != Command.PREFIX)
+      .map((value, index) => `${index + 1}. ${Command.PREFIX}${value}`)
+      .join("\n");
+
+    await this.message.reply(formattedList);
+  }
+}
