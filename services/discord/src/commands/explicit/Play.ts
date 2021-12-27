f69b3b99476ec73a23ae553bf53f0d4bd848ee01
Colin Cheung <ColinCee@users.noreply.github.com>
2021-12-27 16:15:58 +0000

    ADD play command

--- services/discord/src/commands/explicit/Play.ts
+++ services/discord/src/commands/explicit/Play.ts
@@ -1,12 +1,46 @@
 import { AbstractCommand } from "../AbstractCommand";
-import ytdl from "ytdl-core";
-
+import {
+  isMemberInVoiceChannel,
+  joinVoiceChannel
+} from "../../api/discord/VoiceChannel";
+import { VoiceConnection } from "discord.js";
+import { downloadVideo } from "../../api/youtubeDl";
 export class Play extends AbstractCommand {
   async isValid(): Promise<boolean> {
+    if (!(await isMemberInVoiceChannel(this.message))) {
+      await this.message.reply("You must be in a voice channel to play music");
+      return false;
+    }
+    // if url is invalid reply and return false
+    const url = this.message.content.split(" ")[1];
+    if (!url.startsWith("https://www.youtube.com/watch?v=")) {
+      await this.message.reply(
+        "Url should start with https://www.youtube.com/watch?v="
+      );
+      return false;
+    }
+
     return true;
   }
   async run(): Promise<void> {
-    console.log(this.message.content);
-    return;
+    const url = this.message.content.split(" ")[1];
+    try {
+      const videoPath = await downloadVideo(url, this.message);
+      const voiceConnection = await this.getVoiceConnection();
+      await voiceConnection?.play(videoPath);
+    } catch (e) {
+      await this.message.reply("Error downloading video " + e);
+      return;
+    }
+  }
+
+  private async getVoiceConnection(): Promise<VoiceConnection | undefined> {
+    const voiceConnection = await joinVoiceChannel(this.message);
+    if (!voiceConnection) {
+      await this.message.reply("Error getting voice connection");
+      return;
+    }
+
+    return voiceConnection;
   }
 }
